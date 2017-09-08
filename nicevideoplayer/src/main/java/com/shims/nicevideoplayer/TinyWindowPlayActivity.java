3d47a808530d2ad236b9853d912414634dca5eb2
shims <simgsg@163.com>
2017-09-08 18:41:51 +0800

    新增视频播放控件，支持锁屏，拖拽进度，亮度，全屏缩小，挪动等功能

--- nicevideoplayer/src/main/java/com/shims/nicevideoplayer/TinyWindowPlayActivity.java
+++ nicevideoplayer/src/main/java/com/shims/nicevideoplayer/TinyWindowPlayActivity.java
@@ -1,16 +1,13 @@
 package com.shims.nicevideoplayer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-
 import com.bumptech.glide.Glide;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class TinyWindowPlayActivity extends AppCompatActivity {
+public class TinyWindowPlayActivity extends CompatHomeKeyActivity {
     private NiceVideoPlayer mNiceVideoPlayer;
 
     @Override
@@ -48,7 +45,7 @@ public class TinyWindowPlayActivity extends AppCompatActivity {
         clarities.add(new Clarity("蓝光", "1080P", "http://play.g3proxy.lecloud.com/vod/v2/MjQ5LzM3LzIwL2xldHYtdXRzLzE0L3Zlcl8wMF8yMi0xMTA3NjQxMzkwLWF2Yy00MTk4MTAtYWFjLTQ4MDAwLTUyNjExMC0zMTU1NTY1Mi00ZmJjYzFkNzA1NWMyNDc4MDc5OTYxODg1N2RjNzEwMi0xNDk4NTU3OTYxNzQ4Lm1wNA==?b=479&mmsid=65565355&tm=1499247143&key=98c7e781f1145aba07cb0d6ec06f6c12&platid=3&splatid=345&playid=0&tss=no&vtype=13&cvid=2026135183914&payff=0&pip=08cc52f8b09acd3eff8bf31688ddeced&format=0&sign=mb&dname=mobile&expect=1&tag=mobile&xformat=super"));
         return clarities;
     }
-    @Override
+   /* @Override
     protected void onStop() {
         super.onStop();
         NiceVideoPlayerManager.instance().releaseNiceVideoPlayer();
@@ -58,5 +55,5 @@ public class TinyWindowPlayActivity extends AppCompatActivity {
     public void onBackPressed() {
         if (NiceVideoPlayerManager.instance().onBackPressd()) return;
         super.onBackPressed();
-    }
+    }*/
 }
